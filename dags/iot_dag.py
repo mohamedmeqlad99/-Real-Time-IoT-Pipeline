31c33710bdd7689df4e1f0ee4820278181f1f690
mohamedmeqlad99 <mohamedmeqlad9@gmail.com>
2024-09-22 18:32:28 +0300

    change the start date arg so it runs immediately

--- dags/iot_dag.py
+++ dags/iot_dag.py
@@ -8,7 +8,7 @@ import subprocess
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 9, 19),
+    'start_date': datetime.now(),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
