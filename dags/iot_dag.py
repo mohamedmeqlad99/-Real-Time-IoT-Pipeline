a7618b7039b08c41d06cbf9848f5d8bf2041e270
mohamedmeqlad99 <mohamedmeqlad9@gmail.com>
2024-09-22 18:18:07 +0300

    adding args

--- dags/iot_dag.py
+++ dags/iot_dag.py
@@ -3,3 +3,15 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 import os
 import subprocess
+
+#declare the args 
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 9, 19),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
